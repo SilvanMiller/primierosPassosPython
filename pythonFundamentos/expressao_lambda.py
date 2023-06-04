88c2e15e7122b273e64be21ce850c27059198abd
Silvan Miller <silvanmiller@gmail.com>
2023-06-03 21:29:31 -0300

    python Função lambda

--- pythonFundamentos/expressao_lambda.py
+++ pythonFundamentos/expressao_lambda.py
@@ -13,3 +13,23 @@ print(subtrair2(1, 3)) #dando o mesmo resultado de subtrair
 
 subtrair3 = lambda a, b: print(f'resultado: {a - b}')
 subtrair3(1, 3)
+
+lista = [ #dicionario
+    {'produto': 'Fone de ouvido', 'preco': 200},
+    {'produto': 'Controle video game', 'preco': 400},
+    {'produto': 'Celular', 'preco': 1200}
+]
+
+def calcular_desconto(lista, funcao):
+    total = 0
+    for produto in lista:
+        item_desconto = funcao(produto['preco'], 10)
+        print(item_desconto)
+        
+        total += item_desconto 
+        
+        
+    print(f'total: {total}')
+    
+    
+calcular_desconto(lista, lambda a, b: a - b)
