2009cb8074121f8a242440e28061528fbbec6fe7
Silvan Miller <silvanmiller@gmail.com>
2023-06-03 21:22:45 -0300

    python interpolação

--- pythonFundamentos/interpolação.py
+++ pythonFundamentos/interpolação.py
@@ -1,12 +1,12 @@
 CARRO = "Fusca"
 PRECO = 1200.568
 
-#print('carro: ' + CARRO + ', preco: ' + str(PRECO))#concatenação
+print('carro: ' + CARRO + ', preco: ' + str(PRECO))#concatenação
 
 # Versão mais antigas
-#print('carro: %s, preço: %d' % (CARRO, PRECO))#numeros inteiros
-#print('carro: %s, preço: %.2f' % (CARRO, PRECO))#numeros fracionados 
-#print('carro: {0}, preco: {1}'.format(CARRO, PRECO))#um pouco mais nova,
+print('carro: %s, preço: %d' % (CARRO, PRECO))#numeros inteiros
+print('carro: %s, preço: %.2f' % (CARRO, PRECO))#numeros fracionados 
+print('carro: {0}, preco: {1}'.format(CARRO, PRECO))#um pouco mais nova,
 
 # Versão mais modernas
 print(f'carro: {CARRO}, preco: {PRECO}')
