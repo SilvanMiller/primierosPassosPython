1ba165af5dce2e906533efaa1b150301d981855e
Silvan Miller <silvanmiller@gmail.com>
2023-06-11 19:36:04 -0300

    python inputs...

---
+++ pythonFundamentos/funcao_input.py
@@ -0,0 +1,11 @@
+nome = input('Qual seu nome? ')
+print(f'O seu nome e {nome}')
+
+n_1 = input('Digite um número ')
+n_2 = input('Digite outro número ')
+
+int_n1 = int(n_1)
+int_n2 = int(n_2)
+
+#print(f'A soma dos numeros é {n_1 + n_2}')# assim ele concatena pois n_1 2 n_2 são str
+print(f'A soma dos numeros é {int_n1 + int_n2}')# assim ele concatena pois n_1 2 n_2 são str
