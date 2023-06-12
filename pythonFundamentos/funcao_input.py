cbcb60c8d3a9dc0f582dbe9ad7aafd0cb16f1c95
Silvan Miller <silvanmiller@gmail.com>
2023-06-11 21:03:06 -0300

    python inputs...

--- pythonFundamentos/funcao_input.py
+++ pythonFundamentos/funcao_input.py
@@ -1,4 +1,4 @@
-nome = input('Qual seu nome? ')
+#nome = input('Qual seu nome? ')
 #print(f'O seu nome e {nome}')
 
 #n_1 = input('Digite um número ')
@@ -11,12 +11,44 @@ nome = input('Qual seu nome? ')
 #print(f'A soma dos numeros é {int_n1 + int_n2}')# assim ele soma por n1 e n2 sao int agora.
 
 #input com if e else
-entrada = input('Você quer "entrar" ou "sair ' )
+#entrada = input('Você quer "entrar" ou "sair ' )
 
-if entrada == 'entrar':
-    print('Você entrou no Sitema')
-    print(f'Seja bem vindo! {nome}')
-elif entrada == 'sair':
-    print(f'Você saiu do sistema {nome} ')
+#if entrada == 'entrar':
+#    print('Você entrou no Sitema')
+#    print(f'Seja bem vindo! {nome}')
+#elif entrada == 'sair':
+#    print(f'Você saiu do sistema {nome} ')
+#else:
+#    print(f'{nome} Você não digitou algo valido, tente novamente com "entrar" ou #"sair"')
+
+nome = input('Qual seu nome? ')
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor} é maior ou igual ao {segundo_valor}'
+        )
 else:
-    print(f'{nome} Você não digitou algo valido, tente novamente com "entrar" ou "sair"')
+    print(
+        f'{segundo_valor} é maior do que {primeiro_valor}'
+        )
+    
+    
+deseja_somar = input('Deseja somar os valores? " sim" ou " não" ')
+
+if deseja_somar == "sim":
+    primeiro_valor = int(primeiro_valor)
+    segundo_valor = int(segundo_valor)
+    print(
+        f'{nome} A soma dos números : {primeiro_valor} + {segundo_valor}' 
+        f' é {primeiro_valor + segundo_valor}'
+        )
+
+elif deseja_somar == "não":
+    print(
+        f'{nome} então obrigado por sua interação'
+        )
+
+else:
+    print(f'{nome} continuando...')
