75ec5706c1c0b4ed71970f34ca4a94ce0a59fab6
Silvan Miller <silvanmiller@gmail.com>
2023-06-11 21:26:44 -0300

    python inputs...

--- pythonFundamentos/funcao_input.py
+++ pythonFundamentos/funcao_input.py
@@ -10,6 +10,8 @@
 #print(f'A soma dos numeros é {n_1 + n_2}')# assim ele concatena pois n_1 e n_2 são str
 #print(f'A soma dos numeros é {int_n1 + int_n2}')# assim ele soma por n1 e n2 sao int agora.
 
+#---------------------------------------------------------------
+
 #input com if e else
 #entrada = input('Você quer "entrar" ou "sair ' )
 
@@ -21,34 +23,51 @@
 #else:
 #    print(f'{nome} Você não digitou algo valido, tente novamente com "entrar" ou #"sair"')
 
-nome = input('Qual seu nome? ')
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite outro valor: ')
 
-if primeiro_valor >= segundo_valor:
-    print(
-        f'{primeiro_valor} é maior ou igual ao {segundo_valor}'
-        )
-else:
-    print(
-        f'{segundo_valor} é maior do que {primeiro_valor}'
-        )
+#-----------------------------------------------------------------------
+
+#nome = input('Qual seu nome? ')#
+#primeiro_valor = input('Digite um valor: ')#
+#segundo_valor = input('Digite outro valor: ')
+
+#if primeiro_valor >= segundo_valor:
+#    print(
+#        f'{primeiro_valor} é maior ou igual ao {segundo_valor}'
+#        )
+#else:#
+#    print(
+#        f'{segundo_valor} é maior do que {primeiro_valor}'
+#        )
+
+
+#deseja_somar = input('Deseja somar os valores? " sim" ou " não" ')
+
+#if deseja_somar == "sim":#
+#    primeiro_valor = int(primeiro_valor)
+#    segundo_valor = int(segundo_valor)
+#    print(
+#        f'{nome} A soma dos números : {primeiro_valor} + {segundo_valor}' 
+#        f' é {primeiro_valor + segundo_valor}'
+#        )
+
+#elif deseja_somar == "não":
+#    print(
+#        f'{nome} então obrigado por sua interação'
+#        )
+
+#else:
+#    print(f'{nome} continuando...')
+
+
+#-----------Operador lógico AND -------------------------
+
+#entrada = input('[E]entrar [S]sair ')
+#senha_digitada = input('Senha: ')
+#senha_permitida = '123456' #só para teste, nunca a senha fica assim.
+
+#if entrada == 'E' and senha_digitada == senha_permitida:#
+#    print('Entrada permitida')
     
+#else:
+#    print('Sair')
     
-deseja_somar = input('Deseja somar os valores? " sim" ou " não" ')
-
-if deseja_somar == "sim":
-    primeiro_valor = int(primeiro_valor)
-    segundo_valor = int(segundo_valor)
-    print(
-        f'{nome} A soma dos números : {primeiro_valor} + {segundo_valor}' 
-        f' é {primeiro_valor + segundo_valor}'
-        )
-
-elif deseja_somar == "não":
-    print(
-        f'{nome} então obrigado por sua interação'
-        )
-
-else:
-    print(f'{nome} continuando...')
