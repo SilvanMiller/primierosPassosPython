9aca55bebbb11b4426b5383904656108bbdb5f14
Silvan Miller <silvanmiller@gmail.com>
2023-06-15 08:23:56 -0300

    python EXERCICIOS praticando o basico ...

---
+++ exercicios/ex002.py
@@ -0,0 +1,31 @@
+"""
+Faça um programa que leia um número inteiro 
+e moestre na tela seu sucessor e seu antecessor 
+
+Crie um algoritimo que leia um número e mostre 
+o seu dobro, triplo e raiz quadrada
+
+desenvolva um programa que leia as duas notas de 
+um aluno, calcule e mostre a sua media 
+
+escreva um programa que leia um valor em metros e o
+exiba convertido em  centimetros e milimetros 
+
+faça um programa que leia um número inteiro qualquer e 
+mostre a sua tabuada 
+
+crie um programa que leia gnto dinheiro uma pessoa 
+tem na carteira e mostre gntos dólares ela pode comprar 
+dolar a 3,27
+
+faça um programa q leia a largura e a altura de uma parede em metros,
+calcule a sua area e a qntdd de tinta necessária para pinta-lo,
+sabendo que cada litro de tinta pinta uma área de 2m² 
+
+faça um algoritimo q leia o preço de um produto e meostre seu 
+novo preço, com 5% de desconto 
+
+faça um algoritimo que leia o salário de um funcioanrio e 
+mostre seu novo salario com 15% de aumento
+
+"""
