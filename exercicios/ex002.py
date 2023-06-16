708a67c76209eaecc1e94b465502e5e0006e3435
Silvan Miller <silvanmiller@gmail.com>
2023-06-16 10:39:07 -0300

    python EXERCICIOS praticando o basico ...

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,13 +1,4 @@
 """
-Faça um programa que leia um número inteiro 
-e moestre na tela seu sucessor e seu antecessor 
-
-Crie um algoritimo que leia um número e mostre 
-o seu dobro, triplo e raiz quadrada
-
-desenvolva um programa que leia as duas notas de 
-um aluno, calcule e mostre a sua media 
-
 escreva um programa que leia um valor em metros e o
 exiba convertido em  centimetros e milimetros 
 
@@ -29,3 +20,20 @@ faça um algoritimo que leia o salário de um funcioanrio e
 mostre seu novo salario com 15% de aumento
 
 """
+
+print('EX001 - Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.')
+n = int(input('Digite um númro inteiro: '))
+print(f'O número {n} te como seu antecessor o número {n - 1} e seu sucessor é {n + 1}\n\n')
+
+
+print('EX002 - Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada.')
+print(f'O número {n} o seu dobro é {n * 2}, e seu triplo é {n * 3} e sua raiz quadrado é {n ** (1/2):.2f}\n\n')
+
+
+
+print('EX003 - desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua media. ')
+nome = input('Digite o nome do aluno: ')
+nota_1 = float(input(f'Digite a primeira nota do aluno {nome}: '))
+nota_2 = float(input(f'Digite a segunda nota: '))
+print(f'As notas do aluno {nome} são {nota_1} e {nota_2} e a sua media é {(nota_1 + nota_2) / 3}\n\n')
+
