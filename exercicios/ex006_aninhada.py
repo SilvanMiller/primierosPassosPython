b72b4f258f5965b684ecb4113b791ec84ffb9b8a
Silvan Miller <silvanmiller@gmail.com>
2023-07-26 19:23:20 -0300

    Aprendento python...(continue)

--- exercicios/ex006_aninhada.py
+++ exercicios/ex006_aninhada.py
@@ -138,3 +138,24 @@ else:
     print(f'Opção fora dos parametros para converter o número {num}.')
 print('\n\n')
 
+print('\n')
+# -------------------------------
+
+print('EX033 - Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: IMC abaixo de 18,5: Abaixo do Peso - Entre 18,5 e 25: Peso Ideal - 25 até 30: Sobrepeso - 30 até 40: Obesidade - Acima de 40: Obesidade Mórbida\n')
+
+peso = float(input('Digite o sei peso:(kg) '))
+alt = float(input('Digite a sua altura:(metro) '))
+imc = peso / (alt**2)
+#print(f'{imc:.1f}')
+
+if imc < 18.5:
+    print(f'Seu IMC é de {imc:.1f}, e você esta ABAIXO DO PESO.')
+elif 18.5 <= imc < 25.0:
+    print(f'Seu IMC é de {imc:.1f}, e você esta no seu PESO IDEAL Parabéns!!.')
+elif 25 <= imc < 30.0:
+    print(f'Seu IMC é de {imc:.1f}, e você esta SOBREPESO.')
+elif 30 <= imc < 40.0:
+    print(f'Seu IMC é de {imc:.1f}, e você esta com OBESIDADE.')
+else:
+    print(f'Seu IMC é de {imc:.1f}, Cuidado, você esta com OBESIDADE MÓRBIDA.')
+
